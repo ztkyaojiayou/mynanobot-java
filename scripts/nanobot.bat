d6fb2dfbffaa7068c89a63d3677ca97ea1eb2dea
ztkyaojiayou <ztkyaojiayou@users.noreply.github.com>
2026-07-17 18:05:37 +0800

    fix: Windows bat 使用 java @args 文件方式，无 Maven 启动开销

--- scripts/nanobot.bat
+++ scripts/nanobot.bat
@@ -4,5 +4,10 @@ set JAVA_HOME=D:/devSoftWare/jdk17/jdk-17.0.19+10
 set PATH=%JAVA_HOME%\bin;%PATH%
 
 cd /d "%SCRIPT_DIR%"
-call mvn -q compile -DskipTests
-mvn -q spring-boot:run -Dspring-boot.run.mainClass=com.nanobot.v3.NanobotCliApplication -Dspring-boot.run.arguments="%*"
+if not exist "target\classes\com\nanobot\v3\NanobotCliApplication.class" (
+    echo compiling...
+    call mvn compile -q -DskipTests
+)
+
+rem Java @file reads each line as a separate argument, no CMD variable limit
+java "@target\nanobot.args" %*
