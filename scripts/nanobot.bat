e68cf1fb808aaa8ffa386159376d4cadedf66e0d
ztkyaojiayou <ztkyaojiayou@users.noreply.github.com>
2026-07-17 18:16:57 +0800

    feat: 打包 fat JAR (nanobot-cli.jar)，彻底解决 Windows classpath 问题

--- scripts/nanobot.bat
+++ scripts/nanobot.bat
@@ -3,9 +3,9 @@ set SCRIPT_DIR=%~dp0..
 set JAVA_HOME=D:/devSoftWare/jdk17/jdk-17.0.19+10
 set PATH=%JAVA_HOME%\bin;%PATH%
 
-if not exist "%SCRIPT_DIR%\target\classes\com\nanobot\v3\NanobotCliApplication.class" (
-    echo compiling...
-    cd /d "%SCRIPT_DIR%" && call mvn compile -q -DskipTests && cd /d "%CD%"
+if not exist "%SCRIPT_DIR%\target\nanobot-cli.jar" (
+    echo building cli jar...
+    cd /d "%SCRIPT_DIR%" && call mvn package -DskipTests -q
 )
 
-java "@%SCRIPT_DIR%\target\nanobot.args" %*
+java -jar "%SCRIPT_DIR%\target\nanobot-cli.jar" %*
