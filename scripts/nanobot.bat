a277469151a04fa80f5249983b55246ff994ba60
ztkyaojiayou <ztkyaojiayou@users.noreply.github.com>
2026-07-17 18:03:54 +0800

    fix: Windows bat 改用 spring-boot:run 避过 CMD classpath 限制

--- scripts/nanobot.bat
+++ scripts/nanobot.bat
@@ -3,16 +3,6 @@ set SCRIPT_DIR=%~dp0..
 set JAVA_HOME=D:/devSoftWare/jdk17/jdk-17.0.19+10
 set PATH=%JAVA_HOME%\bin;%PATH%
 
-if not exist "%SCRIPT_DIR%\target\classes\com\nanobot\v3\NanobotCliApplication.class" (
-    echo (first run, compiling...)
-    cd /d "%SCRIPT_DIR%" && call mvn compile -q -DskipTests
-)
-
-rem Generate classpath file if it doesn't exist
-if not exist "%SCRIPT_DIR%\target\classpath.txt" (
-    cd /d "%SCRIPT_DIR%" && call mvn dependency:build-classpath -DincludeScope=compile -Dmdep.outputFile=target\classpath.txt -q
-)
-
 cd /d "%SCRIPT_DIR%"
-set /p CP=<target\classpath.txt
-java -cp "target\classes;%CP%" com.nanobot.v3.NanobotCliApplication %*
+call mvn -q compile -DskipTests
+mvn -q spring-boot:run -Dspring-boot.run.mainClass=com.nanobot.v3.NanobotCliApplication -Dspring-boot.run.arguments="%*"
