a8b2763512927ec87ff43c4c5ad5e8caf76b25db
ztkyaojiayou <ztkyaojiayou@users.noreply.github.com>
2026-07-17 18:41:21 +0800

    feat: bash 版 nanobot 自动检测源码更新并重建 JAR，bat 版保持简洁

--- scripts/nanobot.bat
+++ scripts/nanobot.bat
@@ -3,9 +3,11 @@ set SCRIPT_DIR=%~dp0..
 set JAVA_HOME=D:/devSoftWare/jdk17/jdk-17.0.19+10
 set PATH=%JAVA_HOME%\bin;%PATH%
 
-if not exist "%SCRIPT_DIR%\target\nanobot-cli.jar" (
-    echo building...
+set JAR=%SCRIPT_DIR%\target\nanobot-cli.jar
+
+if not exist "%JAR%" (
+    echo (building...)
     pushd "%SCRIPT_DIR%" && call mvn package -DskipTests -q && popd
 )
 
-java -Dloader.main=com.nanobot.v3.NanobotCliApplication -jar "%SCRIPT_DIR%\target\nanobot-cli.jar" %*
+java -Dloader.main=com.nanobot.v3.NanobotCliApplication -jar "%JAR%" %*
